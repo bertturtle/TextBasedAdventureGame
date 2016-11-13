36a3db0d6c0c665b57c84ccaa90f0e5ef1b093aa
bertturtle <mbentley124@gmail.com>
2016-11-12 19:40:37 -0800

    finished

--- src/playerController/Main.java
+++ src/playerController/Main.java
@@ -22,8 +22,12 @@ public class Main {
 		List<Integer> resourceChange = new ArrayList<Integer>();
 		resourceChange.add(3);
 		
+		List<Integer> resourceChange2 = new ArrayList<Integer>();
+		resourceChange2.add(-banana.getResourceQuantity());
+		
 		List<GenericCommand> possibleActions = new ArrayList<GenericCommand>();
-		possibleActions.add(new GenericCommand("talk to gorrila", "talk", new CommandActions(resources, resourceChange), "You talk to the gorilla and he gives you some bananas."));
+		possibleActions.add(new GenericCommand("talk", "communicate", new CommandActions(resources, resourceChange), "You talk to the gorilla and he gives you some bananas."));
+		possibleActions.add(new GenericCommand("attack", "fight", new CommandActions(resources, resourceChange2), "You fight the gorrila and lose, because he is a gorilla. He take all of your bananas, because again he is a gorilla."));
 		GenericSituation situation = new GenericSituation("You run into a gorrila", possibleActions);
 		System.out.println(situation.startSituation());
 	}
